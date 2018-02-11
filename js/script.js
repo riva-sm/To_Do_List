5b32fb3604503fb29b0ca2f9738745d41e34a2a1
Xenia Filippova <xefilip@gmail.com>
2018-02-11 20:34:04 +0800

    Add index.html, add style.css, add script.js

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(function() {
+  var $tasksList = $("#tasksList");
+  var $taskInput = $("#taskInput");
+  var $notification = $("#notification");
+
+  var displayNotification = function() {
+    if (!$tasksList.children().length) { //находит дочерние эл-ты, а потом возвращает число выбранных элементов
+    $notification.fadeIn("fast"); //появляется с быстрой скоростью блок "список задач"
+  } else {
+    $notification.css("display", "none") //в обратном случае вызывается метод, скрывающий блок
+  }
+}
+
+  $("#taskAdd").on("click", function () { //вызываем обработчик событий при нажатии на кнопку +
+    if(!$taskInput.val()) {return false;} //проверяем значение Инпута, возвращаем пустое значение
+
+    $tasksList.append("<li>" + $taskInput.val() + "<button class='delete'>&#10006</button></li>"); //добавляем пункт списка в веденным в поле инпут значением
+                                                                                                    //добавляем кнопку для удаления
+      $taskInput.val(""); //очистка поля для ввода
+
+    displayNotification(); //вызов функции для отображения или скрытия заданий
+
+    $(".delete").on("click", function() {
+      var $parent = $(this).parent();
+
+      $parent.css("animation", "fadeOut .3s linear")
+
+      setTimeout(function() { //устанавливаем время
+        $parent.remove();
+        displayNotification();
+      }, 295);
+    })
+  })
+})
